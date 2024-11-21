057d1092231286893ff4193026a445572dd64010
Shenglin Xu <shenglinxu@microsoft.com>
2024-11-21 17:10:03 +0100

    updated prints

--- web_classification/flows/experiment/convert_to_dict.py
+++ web_classification/flows/experiment/convert_to_dict.py
@@ -6,7 +6,7 @@ from promptflow.core import tool
 @tool
 def convert_to_dict(input_str: str):
     try:
-        print("Convert input to dict: " + input_str)
+    
         return json.loads(input_str)
     except Exception as e:
         print("input is not valid, error: {}".format(e))
